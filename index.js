b9e0086abff9a0708e4b19f7ee3a998a368d3445
SERGEY KOKSHAROV <37808925+KoksharovSA@users.noreply.github.com>
2019-07-18 08:44:22 +0700

    Update index.js

--- index.js
+++ index.js
@@ -27,11 +27,7 @@ class SortBubble {
             clon[y] = [];
             for (let x = 0; x < this.count; x++) {
                 if (y === 1) {
-                    clon[y][x] = mass[y][x];
-                    //Отрисовка склонированного массива в canvas
-                    if (mass[y][x]) {
-                        view.renderBlock(((view.width - (this.count * 45)) / 2) + x * 45, 45, color, mass[y][x]);
-                    }
+                    clon[y][x] = mass[y][x];                   
                 } else {
                     clon[y][x] = 0;
                 }
@@ -61,7 +57,7 @@ class SortBubble {
         console.log(this.massCount);
     }
 
-    //Метод меняющий элементы массива местами шаг 1, принимает контекст, индекс элемента который необходимо поменять и цвет блока для отрисовки
+    //Метод меняющий элементы массива местами шаг 1
     reversCountStepOne(ob, reversElement, color) {
         const mass = ob.notSortWorkMass;
         let temp1 = mass[1][reversElement];
@@ -71,18 +67,10 @@ class SortBubble {
         mass[2][reversElement + 1] = temp2;
         mass[1][reversElement + 1] = 0;
         view.clearCanv();
-        //Отрисовка 1 шага изменения в canvas
-        for (let y = 0; y < 3; y++) {
-            for (let x = 0; x < this.count; x++) {
-                if (ob.notSortWorkMass[y][x]) {
-                    view.renderBlock(((view.width - (this.count * 45)) / 2) + x * 45, y * 45, color, ob.notSortWorkMass[y][x]);
-                }
-            }
-        }
         ob.notSortWorkMass = mass;
     }
 
-    //Метод меняющий элементы массива местами шаг 2, принимает контекст, индекс элемента который необходимо поменять и цвет блока для отрисовки
+    //Метод меняющий элементы массива местами шаг 2
     reversCountStepTwo(ob, reversElement, color) {
         const mass = ob.notSortWorkMass;
         let temp = mass[0][reversElement];
@@ -91,19 +79,10 @@ class SortBubble {
         temp = mass[2][reversElement + 1];
         mass[2][reversElement] = temp;
         mass[2][reversElement + 1] = 0;
-        //Отрисовка 2 шага изменения в canvas
-        view.clearCanv();
-        for (let y = 0; y < 3; y++) {
-            for (let x = 0; x < this.count; x++) {
-                if (ob.notSortWorkMass[y][x]) {
-                    view.renderBlock(((view.width - (this.count * 45)) / 2) + x * 45, y * 45, color, ob.notSortWorkMass[y][x]);
-                }
-            }
-        }
         ob.notSortWorkMass = mass;
     }
 
-    //Метод меняющий элементы массива местами шаг 3, принимает контекст, индекс элемента который необходимо поменять и цвет блока для отрисовки
+    //Метод меняющий элементы массива местами шаг 3
     reversCountStepThree(ob, reversElement, color) {
         const mass = ob.notSortWorkMass;
         let temp = mass[0][reversElement + 1];
@@ -111,33 +90,29 @@ class SortBubble {
         mass[0][reversElement + 1] = 0;
         temp = mass[2][reversElement];
         mass[1][reversElement] = temp;
-        mass[2][reversElement] = 0;
-        //Отрисовка 2 шага изменения в canvas
-        view.clearCanv();
-        for (let y = 0; y < 3; y++) {
-            for (let x = 0; x < this.count; x++) {
-                if (ob.notSortWorkMass[y][x]) {
-                    view.renderBlock(((view.width - (this.count * 45)) / 2) + x * 45, y * 45, color, ob.notSortWorkMass[y][x]);
-                }
-            }
-        }
+        mass[2][reversElement] = 0;        
         ob.notSortWorkMass = mass;
     }
 }
 
 //Класс отвечающий за отображение блоков в canvas
 class View {
-    constructor(element, width, height, blockWidth, blockHeight) {
+    constructor(element, elementWidth, elementHeight) {
         this.element = element; //Элемент DOM в который добавляем canvas
-        this.width = width; //Длина для canvas
-        this.height = height; //Высота для canvas
+        this.scaleX = elementWidth / 600; //Масштаб canvas
+        this.scaleY = elementHeight / 150; //Масштаб canvas
+        console.log(this.scaleX);
+        this.width = 600 * this.scaleX; //Длина для canvas
+        this.height = 150 * this.scaleY; //Высота для canvas
+        console.log(this.width, this.height);
         this.canvas = document.createElement('canvas'); //Создаём элемент canvas
         this.canvas.width = this.width; //Задаём длину canvas 
         this.canvas.height = this.height; //Задаём высоту canvas
         this.context = this.canvas.getContext('2d'); //задаём canvas параметр 2d
 
-        this.blockWidth = blockWidth; //Длина для блоков
-        this.blockHeight = blockHeight; //Ширина для блоков
+        this.blockWidth = 40 * this.scaleX; //Длина для блоков
+        this.blockHeight = 40 * this.scaleY; //Ширина для блоков
+        this.canvas.id = 'canvas';
 
         this.element.appendChild(this.canvas); //Добавляем canvas в элемент DOM
     }
@@ -160,8 +135,21 @@ class View {
         this.context.lineWidth = 2;
         this.context.fillRect(x, y, this.blockWidth, this.blockHeight);
         this.context.strokeRect(x, y, this.blockWidth, this.blockHeight);
-        this.context.font = '18px serif';
-        this.context.strokeText(count, x + 10, y + 25);
+        this.context.font = 18 * this.scaleX + 'px serif';
+        this.context.strokeText(count, x + (10 * this.scaleX), y + (25 * this.scaleY));
+    }
+
+    //Метод отвечающий за отрисовку массива, принимает контекст и цвет
+    renderMass(ob, color) {
+        this.clearCanv();
+        var mass = ob.notSortWorkMass;
+        for (let y = 0; y < mass.length; y++) {
+            for (let x = 0; x < mass[y].length; x++) {
+                if (mass[y][x]) {
+                    this.renderBlock(((this.width - (mass[y].length * (45 * this.scaleX))) / 2) + (x * (45 * this.scaleX)), ((this.height - (mass.length * (45 * this.scaleY))) / 2) + (y * (45 * this.scaleY)), View.colors[color], mass[y][x]);
+                }
+            }
+        }
     }
 
     //Метод отчищающий поле canvas
@@ -172,7 +160,7 @@ class View {
 
 const sortBubble = new SortBubble(); //Создаём экземпляр класса сортировки массивов
 const root = document.querySelector('#root'); //Добовляем переменную и передаём ей сущность необходимого нам элемента DOM
-const view = new View(root, 600, 150, 40, 40); //Создаём экземпляр класса отображения блоков
+view = new View(root, root.clientWidth, root.clientHeight); //Создаём экземпляр класса отображения блоков
 
 speedSort = 9 - document.getElementById('speed').value; //Задаём еременную скорости сортировки
 colorRect = document.getElementById('select').value; //Задаём еременную цвета блоков
@@ -188,7 +176,16 @@ sortBubble.sortMass(sortBubble.workMass); //Сортируе рабочий ма
 
 mas = sortBubble.massCount; //Добовляем переменную и передаём ей массив индексов элементов которые необходимо поменять при сортировке
 count = 0; //Добовляем переменную количества замен элементов произведённых при сортировке
-interval = setInterval(() => {}, 0); //Добовляем переменную для setinterval
+interval = setInterval(() => { }, 0); //Добовляем переменную для setinterval
+view.renderMass(sortBubble, this.colorRect); //Отрисовываем массив
+
+//Действия при изменении размера окна браузера
+window.onresize = () => {
+    root.removeChild(document.getElementById('canvas'));
+    view = new View(root, root.clientWidth, root.clientHeight);
+    console.log(sortBubble.notSortWorkMass, this.colorRect);
+    view.renderMass(sortBubble, this.colorRect);
+};
 
 //Метод изменяющий скорость
 function resizeSpeed() {
@@ -209,6 +206,7 @@ function changed() {
     sortBubble.massCount = []; //Обнуляем массив индексов элементов которые необходимо поменять при сортировке
     sortBubble.createWorkMass(); //Создаём новый рабочий массив
     sortBubble.notSortWorkMass = sortBubble.clonemass(sortBubble.workMass, View.colors[this.colorRect]); //Клонируем в неотсортированный рабочий массив
+    view.renderMass(sortBubble, this.colorRect); //Отрисовываем массив
     sortBubble.sortMass(sortBubble.workMass); //Сортируем рабочий массив
     this.mas = sortBubble.massCount; //Принимаем новый массив индексов элементов которые необходимо поменять при сортировке
 }
@@ -229,18 +227,21 @@ function start() {
             const p1 = new Promise((resolve) => {
                 setTimeout(() => {
                     sortBubble.reversCountStepOne(sortBubble, mas[this.count], View.colors[this.colorRect]);
+                    view.renderMass(sortBubble, this.colorRect);
                 }, this.speedSort * 50);
                 resolve();
             }).then(() => {
                 return new Promise((resolve) => {
                     setTimeout(() => {
                         sortBubble.reversCountStepTwo(sortBubble, mas[this.count], View.colors[this.colorRect]);
+                        view.renderMass(sortBubble, this.colorRect);
                     }, this.speedSort * 100);
                     resolve();
                 });
             }).then(() => {
                 setTimeout(() => {
                     sortBubble.reversCountStepThree(sortBubble, mas[this.count], View.colors[this.colorRect]);
+                    view.renderMass(sortBubble, this.colorRect);
                     this.count += 1;
                 }, this.speedSort * 150);
                 console.log(this.count);
